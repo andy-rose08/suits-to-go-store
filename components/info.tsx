a244a9b0a864ffcd8b9b12c7f5a7e035ee67a75a
andy-rose08 <leznedrios1208@gmail.com>
2024-04-25 11:04:07 -0600

    fix interactive button, preview product

--- components/info.tsx
+++ components/info.tsx
@@ -3,12 +3,18 @@ import { Product } from "@/types";
 import Currency from "@/components/ui/currency";
 import Button from "@/components/ui/button";
 import { BriefcaseBusiness } from "lucide-react";
+import useCart from "@/hooks/use-cart";
 
 interface InfoProps {
   data: Product;
 }
 
 const Info: React.FC<InfoProps> = ({ data }) => {
+  const cart = useCart();
+
+  const onAddToCart = () => {
+    cart.addItem(data);
+  };
   return (
     <div>
       <h1 className="text-3xl font-bold dark:text-white text-black">
@@ -22,15 +28,11 @@ const Info: React.FC<InfoProps> = ({ data }) => {
       <hr className="my-4" />
       <div className="flex flex-col gap-y-6">
         <div className="flex items-center gap-x-4">
-          <h3 className="font-semibold dark:text-white text-black">
-            Size:{" "}
-          </h3>
+          <h3 className="font-semibold dark:text-white text-black">Size: </h3>
           <div className="text-black dark:text-white">{data?.size?.name}</div>
         </div>
         <div className="flex items-center gap-x-4">
-          <h3 className="font-semibold dark:text-white text-black">
-            Color:{" "}
-          </h3>
+          <h3 className="font-semibold dark:text-white text-black">Color: </h3>
           <div
             className="h-6 w-6 rounded-full border border-gray-600"
             style={{ backgroundColor: data?.color?.value }}
@@ -38,7 +40,7 @@ const Info: React.FC<InfoProps> = ({ data }) => {
         </div>
       </div>
       <div className="mt-10 flex items-center gap-x-3">
-        <Button className="flex items-center gap-x-2">
+        <Button onClick={onAddToCart} className="flex items-center gap-x-2">
           Add To Cart
           <BriefcaseBusiness />
         </Button>
